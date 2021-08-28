6691ccf495096e7cb8b58438b21cd4acf6b9aa30
geethadevaiah <67411309+geethadevaiah@users.noreply.github.com>
2021-08-28 15:27:34 +0530

    Update deboucing.js

--- src/7.DebouncingAndThrottling/deboucing.js
+++ src/7.DebouncingAndThrottling/deboucing.js
@@ -1,20 +1,23 @@
 let counter = 0;
+let timer; // get the timer to clear later
+
 const getData = function(){
     console.info("get data ", counter++);
 }
 
 // debounce call
 const debounce = function(fn, delay){
-    let timer; // get the timer to clear later
+    
     return function(){
         let obj = this, // get the current context
         args = arguments; // get the arguments
         clearTimeout(timer); // clear previous timeouts
         timer = setTimeout(function(){
             fn.apply(obj, arguments); // apply the main call with arguments
-        }, delay); // n dela
+        }, delay); // n delay
     }
 }
 
 // on key up call 
+// call debounce of getData only when there is 300ms time diff in keypress 
 const onKeyupSearchFunction = debounce(getData, 300);
